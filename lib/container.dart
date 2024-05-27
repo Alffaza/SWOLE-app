694bdcfff512aba7f82a8fa3ef4e07753ccd8927
Ryo Hilmi <ryodm.74@gmail.com>
2024-05-27 13:01:56 +0000

    feat: page container

---
+++ lib/container.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+
+class PageContainer extends StatefulWidget {
+  const PageContainer({super.key});
+
+  @override
+  State<PageContainer> createState() => _PageContainerState();
+}
+
+class _PageContainerState extends State<PageContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Exercise',
+          ),
+          
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        // currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        // onTap: _onItemTapped,
+      ),
+    );
+  }
+}
