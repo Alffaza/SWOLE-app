ae91c5fe1354ba3b4f5cf0557b8e2ddcafb5bc7f
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2024-05-28 16:03:51 +0700

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:swole_app/container.dart';
+import 'package:swole_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
